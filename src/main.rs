38158974955bd46720f59b2c7cd77b1fb95a85d2
FlyWolfe <mjsherry248@gmail.com>
2024-06-08 22:19:48 -0400

    Initial 3D Scene + Camera + Character

---
+++ src/main.rs
@@ -0,0 +1,107 @@
+use bevy::prelude::*;
+use bevy_egui::{egui, EguiContexts, EguiPlugin};
+use bevy_xpbd_3d::prelude::*;
+use character_controller::*;
+use smooth_bevy_cameras::{
+    controllers::unreal::{UnrealCameraBundle, UnrealCameraController, UnrealCameraPlugin},
+    LookTransformPlugin,
+};
+
+mod character_controller;
+
+fn main() {
+    App::new()
+        .insert_resource(Msaa::default())
+        .add_plugins((
+            DefaultPlugins,
+            LookTransformPlugin,
+            UnrealCameraPlugin::default(),
+            PhysicsPlugins::default(),
+            EguiPlugin,
+            CharacterControllerPlugin,
+        ))
+        .add_systems(Startup, setup)
+        // Systems that create Egui widgets should be run during the `CoreSet::Update` set,
+        // or after the `EguiSet::BeginFrame` system (which belongs to the `CoreSet::PreUpdate` set).
+        .add_systems(Update, ui_example_system)
+        .run();
+}
+
+fn ui_example_system(mut contexts: EguiContexts) {
+    if true {
+        return;
+    }
+    egui::Window::new("Hello").show(contexts.ctx_mut(), |ui| {
+        ui.label("world");
+    });
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    assets: Res<AssetServer>,
+) {
+    // Player
+    commands.spawn((
+        PbrBundle {
+            mesh: meshes.add(Capsule3d::new(0.4, 1.0)),
+            material: materials.add(Color::rgb(0.8, 0.7, 0.6)),
+            transform: Transform::from_xyz(0.0, 1.5, 0.0),
+            ..default()
+        },
+        CharacterControllerBundle::new(Collider::capsule(1.0, 0.4)).with_movement(
+            100.0,
+            0.92,
+            8.0,
+            (30f32).to_radians(),
+        ),
+        Friction::ZERO.with_combine_rule(CoefficientCombine::Min),
+        Restitution::ZERO.with_combine_rule(CoefficientCombine::Min),
+        GravityScale(2.0),
+    ));
+
+    // A cube to move around
+    commands.spawn((
+        RigidBody::Dynamic,
+        Collider::cuboid(1.0, 1.0, 1.0),
+        PbrBundle {
+            mesh: meshes.add(Cuboid::default()),
+            material: materials.add(Color::rgb(0.8, 0.7, 0.6)),
+            transform: Transform::from_xyz(3.0, 2.0, 3.0),
+            ..default()
+        },
+    ));
+
+    // Environment (see `async_colliders` example for creating colliders from scenes)
+    commands.spawn((
+        SceneBundle {
+            scene: assets.load("character_controller_demo.glb#Scene0"),
+            transform: Transform::from_rotation(Quat::from_rotation_y(-std::f32::consts::PI * 0.5)),
+            ..default()
+        },
+        AsyncSceneCollider::new(Some(ComputedCollider::ConvexHull)),
+        RigidBody::Static,
+    ));
+
+    // Light
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 2_000_000.0,
+            range: 50.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(0.0, 15.0, 0.0),
+        ..default()
+    });
+
+    commands
+        .spawn(Camera3dBundle::default())
+        .insert(UnrealCameraBundle::new(
+            UnrealCameraController::default(),
+            Vec3::new(-2.0, 5.0, 5.0),
+            Vec3::new(0., 0., 0.),
+            Vec3::Y,
+        ));
+}
